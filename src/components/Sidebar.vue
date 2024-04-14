6194f6bb40b3d0cc140e10e9cabd97e4d24bd1cc
roma_marshall <roma_marshall@outlook.com>
2024-04-14 19:55:06 +0200

    add icons svg

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,63 +3,63 @@
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <button @click="showMenu()" data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
         <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_LG"> <path id="Vector" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+        <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_LG"> <path id="Vector" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke="#6A7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
       </button>
       <ul class="space-y-2 font-medium">
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75">
               <use xlink:href="../assets/icons/icons.svg#slots" />
             </svg>
-            <span class="">Slots</span>
+            <span>Slots</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
-              <use xlink:href="../assets/icons/icons.svg#slots" />
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75">
+              <use xlink:href="../assets/icons/icons.svg#live" />
             </svg>
-            <span class="">Live</span>
+            <span>Live</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
-              <use xlink:href="../assets/icons/icons.svg#slots" />
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75" fill="#6A7280" viewBox="0 0 122.88 122.88">
+              <use xlink:href="../assets/icons/icons.svg#bets" />
             </svg>
-            <span class="">Bets</span>
+            <span>Bets</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
-              <use xlink:href="../assets/icons/icons.svg#slots" />
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75" fill="none" viewBox="0 0 24 24">
+              <use xlink:href="../assets/icons/icons.svg#bonus" />
             </svg>
-            <span class="">Bonus</span>
+            <span>Bonus</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
-              <use xlink:href="../assets/icons/icons.svg#slots" />
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75" fill="none" viewBox="0 0 24 24">
+              <use xlink:href="../assets/icons/icons.svg#ranking" />
             </svg>
-            <span class="">Ranking</span>
+            <span>Ranking</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
-              <use xlink:href="../assets/icons/icons.svg#slots" />
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75" fill="none" viewBox="0 -0.5 25 25">
+              <use xlink:href="../assets/icons/icons.svg#refs" />
             </svg>
-            <span class="">Referrals</span>
+            <span>Referrals</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg aria-hidden="true" class="flex-none w-16 h-16">
-              <use xlink:href="../assets/icons/icons.svg#slots" />
+            <svg aria-hidden="true" class="flex-none w-16 h-16 scale-75" fill="#6A7280" viewBox="0 0 512 512">
+              <use xlink:href="../assets/icons/icons.svg#support" />
             </svg>
-            <span class="">Help</span>
+            <span>Help</span>
           </a>
         </li>
       </ul>
